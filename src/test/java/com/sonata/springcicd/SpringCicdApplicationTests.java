670b5ed41537b7b292d9215bec8fc5af4b3b739f
Monika M S <monika.s@sonata-software.com>
2023-09-26 23:06:41 +0530

    my first commit3

---
+++ src/test/java/com/sonata/springcicd/SpringCicdApplicationTests.java
@@ -0,0 +1,14 @@
+package com.sonata.springcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCicdApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+	String console(){
+		return "TestCases Successful";
+	}
+}
